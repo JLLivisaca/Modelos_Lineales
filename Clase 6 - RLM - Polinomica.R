f39e6f278265a51a5d9e7274c8ff979a8d427722
JLLivisaca <jllivisaca@users.noreply.github.com>
2024-03-06 17:41:36 -0500

    Update Clase 6 - RLM - Polinomica.R

--- Clase 6 - RLM - Polinomica.R
+++ Clase 6 - RLM - Polinomica.R
@@ -68,7 +68,8 @@ plot(mfull.interaccion, which=4, cook.levels=cutoff, las=1)
 abline(h=cutoff, lty="dashed", col="blue")
 
 #### Regresión Polinómica ####
-# Vamos a considerar la relación entre la concentración y la resistencia de cemento
+
+# Vamos a considerar la relación entre la concentración de madera dura (%) y la resistencia del papel medida en psi
 conc <- c(1, 1.5, 2, 3, 4, 4.5, 5, 5.5, 6, 6.5, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 resis <- c(6.3, 11.1, 20, 24, 26.1, 30, 33.8, 34, 38.1, 39.9, 42, 46.1, 53.1, 
            52, 52.5, 48, 42.8, 27.8, 21.9)
@@ -79,67 +80,46 @@ ggplot(datos, aes(x=Concentración, y=Resistencia)) +
 
 # De este diagrama se ve claramente que hay una relación de tipo no lineal entre las variables. 
 # ¿Será mejor un modelo de grado 2 que un modelo de grado 1?
+
+mod1 <- lm(Resistencia ~ Concentración, data=datos) # Modelo grado 1
+mod2 <- lm(Resistencia ~ Concentración + I(Concentración^2), data=datos) # Modelo grado 2, la función I(x^2) indica el grado del polinomio
+# Graficos 
+ggplot(datos, aes(x=Concentración, y=Resistencia)) + 
+  geom_point() +
+  geom_smooth(method='lm', formula=y~x, se=FALSE, col='dodgerblue1') +
+  geom_smooth(method='lm', formula=y~x+I(x^2), se=FALSE, col='tomato') +
+  theme_light()
+
+# Resumen modelo 
+summary(mod1)
+summary(mod2)
+anova(mod1)
+anova(mod2)
+anova(mod1, mod2) # Comparando los residuos de los dos modelos, nos indica que el segundo es el mejor
+# Grafica residuos 
+par(mfrow=c(1, 2))
+plot(mod1, which=1, caption='Modelo lineal')
+plot(mod2, which=1, caption='Modelo cuadratico')
+par(mfrow=c(1, 1))
+# Si aumento el polinomio, ¿qué ocurre?
+mod3 <- lm(Resistencia ~ Concentración + poly(Concentración,degree=3), data=datos)
+summary(mod3)
+# Grafica de los modelos con grado 2 a 5
+ggplot(datos, aes(x=Concentración, y=Resistencia)) + 
+  geom_point() +
+  geom_smooth(method='lm', formula=y~x, se=FALSE, col='dodgerblue1') +
+  geom_smooth(method='lm', formula=y~x+I(x^2), se=FALSE, col='tomato') +
+  geom_smooth(method='lm', formula=y~x+I(x^2)+I(x^3), se=FALSE, col='gold') +
+  geom_smooth(method='lm', formula=y~x+I(x^2)+I(x^4), se=FALSE, col='green') +
+  geom_smooth(method='lm', formula=y~x+I(x^2)+I(x^4)+I(x^5), se=FALSE, col='purple')+
+  theme_light()
+
+# Seleccionar variables 
+step(mod2)
+
+# Cuidado con el ajuste del modelo. 
+
+# Colinealidad en las variables
 library(car)
-
-
-library(readr)
-datos1 <- read_table2("C:/Users/Juan Llivisaca/OneDrive/Escritorio/Maestria Estadística/3 Modelos Lineales/Bases de datos/acaros.txt")
-datos1 <- read_table("Maestria Estadística/3 Modelos Lineales/Bases de datos/acaros.txt")
-attach(datos1)
-datos1=as.data.frame(datos1)
-#Diagrama de dispersion
-plot(datos1)
-
-#Polinomio de grado cero
-modelo0<-lm(datos1$acaros~1)
-abline(modelo0,lwd=2,lty=2)
-
-#Polinomio de grado uno
-modelo1<-lm(datos1$acaros~humedad)
-summary(modelo1)
-abline(modelo1,lwd=2,col="blue")
-#EL AJUSTE NO ES BUENO, EL BETA CERO SE ANULA
-
-#Polinomio de grado dos
-humedad2=humedad*humedad # es la variable al cuadrado
-modelo2<-lm(datos1$acaros~humedad+humedad2)
-modelo2<-lm(datos1$acaros~humedad+I(humedad^2)) # es la mejor forma esta 
-# el colocar el valor de I y poner al cuadrado I(humedad^2)
-summary(modelo2)
-beta=coef(modelo2)
-curve(beta[1]+beta[2]*x+beta[3]*x^2,add=TRUE,lwd=2,col="red")
-#LOS BETAS SON SIGNIFICATIVOS, EL AJUSTE ES MEJOR
-#OJO- LA FUNCION CURVE ES PARA GRAFICAR FUNCIONES GENERICAS
-
-#PREDICCION PARA UNA HUMEDAD DEL 80%
-#hay que fijarse en las escalas de las variables (% o escala de 1)
-Ypred0<-beta[1]+beta[2]*80+beta[3]*(80)^2 # cuando uso la variable ficticia de la linea 21
-Ypred0
-predict(modelo2,data.frame(humedad=80)) # OJO:solo funciona porque usamos el comando I en la regresion
-
-#Polinomio de grado tres
-modelo3<-lm(datos1$acaros~humedad+I(humedad^2)+I(humedad^3))
-summary(modelo3)
-beta=coef(modelo3)
-curve(beta[1]+beta[2]*x+beta[3]*x^2+beta[4]*x^3,add=TRUE,lwd=2,lty=2,col="green")
-#CUANDO SE SOBREESTIMAN LOS PARAMETROS SE PIERDEN SIGNIFICACION DE LOS BETAS.
-#OBSERVE QUE EL VALOR DE F ES SIGNIFICATIVO (el pe valor del modelo es menor a alfa), LO QUE INDICA QUE GLOBALMENTE ESTA BIEN
-#PERO INDIVIDUALMENTE LOS BETAS SON NULOS
-# esto es un sobreajuste
-
-# pimer paso fijar el k=3
-summary(modelo3) #sobreajuste lo elimino
-summary(modelo2)# funciona mejor
-#podría buscar algún modelo que se ajuste mejor, bajo el k
-summary(modelo1) 
-# como se ve en el modelo uno, se elimina un betha, y reduce la 
-#bondad de ajuste
-
-#conclusión se queda con el modelo 2, porque me conviene por ajuste y significancia
-# Encontrar el mejor modelo con AIC
-step(modelo3)
-# Encontrar el VIF para la colinealidad
-
-
-vif(modelo)
-
+vif(mod2)
+vif(mod3)
